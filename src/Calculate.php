be7a2a83c40cfe3a0b3b8f910c6fa0e2c3ed2dab
Dehn, Katharina <k.dehn@trio-group.de>
2023-04-05 15:48:54 +0200

    initial commit

---
+++ src/Calculate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Katharinadehn\Calculator;
+
+class Calculate {
+    public function add($x, $y) {
+        return $x + $y;
+    }
+
+    public function multiply($x, $y) {
+        return $x * $y;
+    }
+
+    public function subtrack($x, $y) {
+        return $x - $y;
+    }
+
+    public function divide($x, $y) {
+        return $x / $y;
+    }
+}
+
